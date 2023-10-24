46dd7c84afcda0e75cf349d464d0381ef8454687
Alexsander Rubens <89158806+rubensalexsander@users.noreply.github.com>
2023-10-24 20:02:43 -0300

    commit

--- arSqlite.py
+++ arSqlite.py
@@ -50,81 +50,6 @@ class DbSqlite:
         self.disconnect()
         return lista
     
-    #Edição de tabelas---------------------------------------------------------
-    def new_table(self, name:str, columns:tuple):
-        cursor = self.connect().cursor()
-        cursor.execute(f"""
-        CREATE TABLE {name} {str(columns).replace("'", "")};""")
-        self.disconnect()
-    
-    def new_table_column(self, table_name:str, column_name:str, index:int=-1, standard_value=''):
-        columns = self.get_table_columns(table_name)
-        if (index > len(columns)) or (-index > len(columns) + 1):
-            return print(f'Index "{index}" not accepted') 
-        if column_name in columns:
-            return print(f'The "{column_name}" column already exists.')
-        if index == -1: columns.append(column_name)
-        else:
-            if index < -1: columns.insert(index+1, column_name)
-            else: columns.insert(index, column_name)
-        if type(standard_value) == type([]) and (not len(standard_value) == len(self.get_table(table_name))):
-            return print(f'The list of values ​​is not properly sized({len(self.get_table(table_name))})!')
-        #Salvar table antes e column antes
-        table_before = self.get_table(table_name)
-        #Excluir table
-        self.del_table(table_name)
-        #Criar nova tabela
-        self.new_table(table_name, tuple(columns))
-        #Adicionar valores a nova tabela
-        for i in range(len(table_before)):
-            x = list(table_before[i])
-            if type(standard_value) == type([]): val = standard_value[i]
-            else: val = standard_value
-            val = str(val)
-            if index == -1: x.append(val)
-            else:
-                if index < -1: x.insert(index+1, val)
-                else: x.insert(index, val)
-            self.new_instance(table_name, x)
-    
-    #Excluir coluna de uma tabela
-    def del_table_column(self, table_name:str, column_name:str):
-        cursor = self.connect().cursor()
-        cursor.execute(f"ALTER TABLE {table_name} DROP COLUMN {column_name}")
-        self.disconnect()
-    
-    #Reordenar coluna
-    def reorder_column(self, table_name:str, column_name:str, index_after:int):
-        columns = self.get_table_columns(table_name)
-        columns.remove(column_name)
-        if (index_after > len(columns)) or (-index_after > len(columns) + 1):
-            return print(f'Index "{index_after}" not accepted') 
-        index_before = self.get_table_columns(table_name).index(column_name)
-        column_data = [i[index_before] for i in self.get_table(table_name)]
-        #Excluir coluna
-        self.del_table_column(table_name, column_name)
-        #Criar a coluna novamente
-        self.new_table_column(table_name, column_name, index_after, standard_value=column_data)
-    
-    #Editar colunas de uma tabela
-    def edit_table_columns(self, name:str, columns:tuple):
-        table_actual_columns = self.get_table_columns(name)
-        add_columns = []
-        del_columns = []
-        for i in table_actual_columns:
-            if not i in columns: del_columns.append(i)
-        for i in columns:
-            if not i in table_actual_columns: add_columns.append(i)
-        for i in add_columns: self.new_table_column(name, i)
-        for i in del_columns: self.del_table_column(name, i)
-
-    #Exclui uma tabela
-    def del_table(self, name:str):
-        cursor = self.connect().cursor()
-        cursor.execute(f"DROP TABLE IF EXISTS {name}")
-        self.disconnect()
-    #--------------------------------------------------------------------------
-    
     #Cria nova instância de uma tabela. 
     def new_instance(self, table:str, tupla:tuple):
         conn = self.connect()
@@ -152,9 +77,7 @@ class DbSqlite:
         conn.commit()
 
 def main():
-    db = DbSqlite('db_test.db')
-    columns = ('id', 'idade', 'teste', 'column0003')
-    #db.edit_table_columns('table02', columns)
+    pass
     
 if __name__ == '__main__':
     main()
